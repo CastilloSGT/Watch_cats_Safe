9b3e6723bbb7a2e838fa4d508ef796a26d2658b7
oibene <alvespbene@gmail.com>
2023-03-14 21:38:12 -0300

    tentativa de dialogo e animações bonitinhas

--- root/src/ingame/cenario/casa/objetos.gd
+++ root/src/ingame/cenario/casa/objetos.gd
@@ -1,17 +1,45 @@
 extends Area2D
 
-var onarea = false
+# COLISAO?
+var onareaComp = false
+var onareaCama = false
+
+onready var computador = $Computador
+onready var programando = $Programando
+onready var player = $"../../player"
+
+onready var cama_animation = $Animation
+onready var janela_animation = $"../janelas/Animation"
 		
 func hackeando():
 	get_tree().change_scene("res://src/ingame/stage/minigame1/rat-attack.tscn")
 
 func _physics_process(_delta: float) -> void:
 	if(Input.is_action_just_pressed("ui_select")):
-		if (onarea == true):
+		if (onareaComp == true):
 			hackeando()
 	
 func _on_computador_body_exited(body):
-	onarea = false
+	onareaComp = false
+	programando.hide()
+	computador.show()
+	player.visible = true
 	
 func _on_computador_body_entered(body):
-	onarea = true
+	onareaComp = true
+	programando.show()
+	computador.hide()
+	player.visible = false
+	
+func _on_cama_body_entered(body):
+	onareaCama = true
+	cama_animation.play("dormindo")
+	janela_animation.play("noite")
+	player.visible = false
+
+
+func _on_cama_body_exited(body):
+	onareaCama = false
+	cama_animation.play("vazia")
+	janela_animation.play("dia")
+	player.visible = true
---
+++ root/src/ingame/dialogue/dialogue_area.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+export var dialog_key = ""
+
+var area_active = false
+
+func _input(event):
+	if (area_active and Input.is_action_pressed("ui_accept")):
+		SignalBus.emit_signal("display_dialog", dialog_key)
+
+
+func _on_dialogue_area_area_entered(area):
+	area_active = true
+
+
+func _on_dialogue_area_area_exited(area):
+	area_active = false
