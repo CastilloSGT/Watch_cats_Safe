b0518dc3ed95b73c52c6fba59338e8482446b8ea
oibene <alvespbene@gmail.com>
2023-03-23 16:38:50 -0300

    começo de animação (falhou brutalmente)

--- root/src/ingame/stage/minigame2/fighter.gd
+++ root/src/ingame/stage/minigame2/fighter.gd
@@ -3,11 +3,12 @@ extends KinematicBody2D
 var velocidade: Vector2
 export(int) var speed = 60 #var para mover personagem
 
-onready var animado = $animation
+onready var estado_animado = $AnimationTree
+onready var vetor = Vector2(0,0)
 
 func _physics_process(_delta: float) -> void: #roda durante todo nosso jogo
 	mexe()
-	socos()
+	animacao()
 	
 # move personagem
 func mexe() -> void:
@@ -15,17 +16,19 @@ func mexe() -> void:
 	- Input.get_action_strength("ui_left"), 0)
 	
 	if direcao != Vector2.ZERO:
-		animado.play(("walk"))
+		vetor = Vector2(5,0)
 	else:
-		animado.play("idle")
+		vetor = Vector2(4,0)
 		
 	velocidade = direcao * speed
 	velocidade = move_and_slide(velocidade)
 	
-func socos():
-	if Input.is_action_just_pressed("soco"):
-		animado.play("soco")
-	if Input.is_action_just_pressed("soco-forte"):
-		animado.play("soco-forte")
-	if Input.is_action_just_pressed("desvia"):
-		animado.play("desvio")
+func animacao():
+	estado_animado.set("parameters/estado/blend_position", vetor);
+	print(vetor)
+	if (Input.is_action_just_pressed("desvia")):
+		vetor = Vector2(1,0)
+	if (Input.is_action_just_pressed("soco")):
+		vetor = Vector2(2,0)
+	if (Input.is_action_just_pressed("soco-forte")):
+		vetor = Vector2(3,0)
