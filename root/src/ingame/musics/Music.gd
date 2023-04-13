f1a2bcf550b12b11abec4e861f5529d014e8f3a4
Js <saulgabriel.ct@gmail.com>
2023-04-13 16:51:10 -0300

    confg

--- root/src/ingame/musics/Music.gd
+++ root/src/ingame/musics/Music.gd
@@ -4,11 +4,19 @@ var batids_menu = load("res://src/ingame/musics/musics/menu.ogg")
 
 func _ready():
 	pass
-	
+	 
 func play_music(botas):
 	$Musics.stream = batids_menu
+	$Musics.play()
 	if botas == 1:
-		$Musics.play()
+		$Control.visible = false
+		$TextureRect.visible =false
 	else:
 		$Musics.stop()
-	
+
+func _on_Mute_toggled(button_pressed):
+	AudioServer.set_bus_mute(0, !button_pressed)
+
+
+func _on_btnVoltar_pressed():
+	get_tree().change_scene("res://src/interface/menu.tscn")
--- root/src/interface/menu.gd
+++ root/src/interface/menu.gd
@@ -1,19 +1,33 @@
 extends Control
 onready var btnStart = $menu/btn_start
 var entrou = false
+var opts = false
 
 func _ready():
 	btnStart.grab_focus()
 	Music.play_music(1)
+	
 
 func _process(delta):
 	if Input.is_action_pressed("ui_select"):
 		if (entrou == true):
-			Music.play_music(0)
+			#Music.play_music(1)
 			get_tree().change_scene("res://src/ingame/cenario/casa/quarto.tscn")
+		elif opts == true && entrou == false:
+			get_tree().change_scene("res://src/ingame/musics/Music.tscn")
+		else:
+			return;
+	
+	
 
 func _on_btn_start_focus_entered():
 	entrou = true
 
 func _on_btn_start_focus_exited():
 	entrou = false
+
+func _on_btn_options_focus_entered():
+	opts = true
+
+func _on_btn_options_focus_exited():
+	opts = false
