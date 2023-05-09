75307926aef342824aaf0e5c55934f558ef1d37e
oibene <alvespbene@gmail.com>
2023-05-09 12:48:10 -0300

    macacos saltitantes eu consegui denovo!!!!!

--- root/src/ingame/stage/minigame2/monkey-out.gd
+++ root/src/ingame/stage/minigame2/monkey-out.gd
@@ -2,10 +2,7 @@ extends Node2D
 
 onready var _round = $legendas/lblRound
 onready var tempo = $legendas/rounds
-onready var intervalo = $legendas/intervalo
-
-onready var _modulate = $modulate
-onready var status = $"label-colorida"
+onready var intervalo = $intervalo
 
 var PRE_inimigo = preload("res://src/ingame/stage/minigame2/fighters/enemy.tscn")
 
@@ -15,31 +12,14 @@ var _position
 
 func _ready():
 	Global.pos_fighter = $fighters/position.global_position
-	tempo.wait_time = 10
-	tempo.start()
 
 func _process(delta):
-	getRound()
 	contagem()
-	
-	var enemies = get_tree().get_nodes_in_group("enemies")
-	if(enemies.size() < round_atual):
-		invocaMonkey(round_atual)
+	_round.set_text(str("Round ",round_atual))
 
-func getRound():
-	match round_atual:
-		1:
-			_round.set_text(str("Round 1"))
-		2:
-			_round.set_text(str("Round 2"))
-		3:
-			_round.set_text(str("Round 3"))
-		0:
-			_round.set_text(str("Jogo Finalizado"))
-			
 func rounds():
-	intervalo.start()
-	if (round_atual == 3):
+	$intervalo/intervalo.start()
+	if (round_atual == 5):
 		round_atual = 0
 		get_tree().change_scene("res://src/interface/fim_prototipo.tscn")
 	
@@ -49,15 +29,19 @@ func contagem():
 	var msg = "%02d:%02d" % [minutes, seconds]
 	$legendas/lblTimer.set_text(msg)
 
-		
 func _on_tutorial_timer_timeout():
 	$tutorial.hide()
+	invocaMonkey()
+	
+	tempo.start()
+	$fighters/delay.start()
+	
 
-func invocaMonkey(num):
+func invocaMonkey():
 	var enemies = get_tree().get_nodes_in_group("enemies").size()
 	var inimigo = PRE_inimigo.instance()
 	
-	if(enemies < num):
+	if(enemies != 3):
 		get_parent().add_child(inimigo)
 		inimigo.position = Global.pos_fighter
 
@@ -71,16 +55,10 @@ func _on_rounds_timeout():
 	rounds()
 	round_atual += 1
 	
-	print(round_atual)
-	
-	_modulate.show()
-	status.show()
+	intervalo.show()
 	get_tree().paused = true
-	$fighters/fighter.hide()
 	
 func _on_intervalo_timeout():
-	_modulate.hide()
-	status.hide()
-	$fighters/fighter.show()
+	invocaMonkey()
+	intervalo.hide()
 	get_tree().paused = false
-	invocaMonkey(round_atual)
