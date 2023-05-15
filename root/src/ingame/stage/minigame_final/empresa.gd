8c6f52a29f686909c1e8cd8ddc164f31f6508701
Js <saulgabriel.ct@gmail.com>
2023-05-15 15:01:46 -0300

    Menu Pause durante o jogo

--- root/src/ingame/stage/minigame_final/empresa.gd
+++ root/src/ingame/stage/minigame_final/empresa.gd
@@ -1,3 +1,21 @@
 extends Node2D
 
+func _ready():
+	pass
+
+func _ler():
+	var data_to_send = ["a", "b", "c"]
+	var json_string = JSON.stringify(data_to_send)
+	# Save data
+	# ...
+	# Retrieve data
+	var error = json.parse(json_string)
+	if error == OK:
+		var data_received = json.data
+		if typeof(data_received) == TYPE_ARRAY:
+			print(data_received) # Prints array
+		else:
+			print("Unexpected data")
+else:
+	print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
 
