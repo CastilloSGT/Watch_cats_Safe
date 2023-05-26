7d6cc9048e8b457cd798239361f81410954ab91e
oibene <alvespbene@gmail.com>
2023-05-26 10:45:43 -0300

    commit de segurança

--- root/src/ingame/stage/minigame2/fighters/minion.gd
+++ root/src/ingame/stage/minigame2/fighters/minion.gd
@@ -8,6 +8,13 @@ signal nocateado()
 var attack = false
 var caiu = false
 var area_enemy = false
+var ready = false
+
+# TIMER
+onready var my_timer = get_node("Timer")
+var wait_time = 0
+var reduction = 0.2
+var is_timer_running = false #sensor
 
 var posLeft: Vector2
 var posRight: Vector2
@@ -15,36 +22,47 @@ var inital_pos: Vector2
 
 func _ready():
 	inital_pos = self.position
+	animation.play("idle")
+	$"recover-time".start()
 
 func _physics_process(_delta: float) -> void:
 	animacao(_delta)
+	_on_Timer_timeout()
 	
-	if($"../../fighter".position.y < -3):
-		if(self.name == "minion_center" && $"../../fighter".position.x > -20 && position.x < 20):
-			attack = true
-		if(self.name == "minion_left" && $"../../fighter".position.x <= -20):
-			attack = true
-		if(self.name == "minion_right" && $"../../fighter".position.x >= 20):
-			attack = true
-		
+	if(ready):
+		if($"../../fighter".position.y < 10):
+				if(self.name == "minion_center" && $"../../fighter".position.x > -20 && position.x < 20):
+					attack = true
+				if(self.name == "minion_left" && $"../../fighter".position.x <= -20):
+					attack = true
+				if(self.name == "minion_right" && $"../../fighter".position.x >= 20):
+					attack = true
+	
 	if !caiu:
 		getPos(_delta)
 	
 func animacao(delta):
-	if (Global.tipo_dano != 1 && Global.tipo_dano != 2) && !caiu:
+	if (Global.tipo_dano != 1 && Global.tipo_dano != 2) && !caiu && !is_timer_running:
 		animation.play("idle")
 		
 	if (area_enemy):
-		if (Global.tipo_dano == 1 || Global.tipo_dano == 2) && !caiu:
+		if (Global.tipo_dano == 1 || Global.tipo_dano == 2) && !caiu && !is_timer_running:
 			animation.play("desmaio")
 			caiu = true
 			emit_signal("nocateado")
-
+			
 func getPos(delta):	
-	var target = Vector2($"../../fighter".position.x, 3)
-	
+	var target = Vector2($"../../fighter".position.x, 10)
 	if attack:
-		animation.play("atacando")
+		#nao reseta a animação sla
+		if(animation.get_current_animation() ==  'idle'):
+			animation.play("atacando") 
+			wait_time = 15
+		
+		if(area_enemy):
+			#roda muito mas dboa nao vo criar outro sensor
+			Global.vida_fighter -= .5
+			
 		#faz uma curvinha engraçadinha
 		if(self.name == "minion_left"):
 			posLeft = self.position
@@ -58,13 +76,32 @@ func getPos(delta):
 			$"ataque-delay".start()
 			caiu = true
 	else:
-		animation.play("levantando")
-		yield(animation,"animation_finished")
 		self.global_position = self.global_position.move_toward(inital_pos, delta * speed)
 	
 func _on_ataquedelay_timeout():
-	caiu = false
+	animation.play("levantando")
+	yield(animation, "animation_finished")
+	wait_time = 15
+	
+	#reset vars
 	attack = false
+	caiu = false
+	$"recover-time".start()
+	ready = false
+
+func _on_Timer_timeout():
+	wait_time -= reduction
+	
+	if(wait_time > 0.0):
+		my_timer.set_wait_time(wait_time)
+		is_timer_running = true
+	else:
+		wait_time = 0
+		Global.tipo_dano = -1
+		is_timer_running = false
+
+func _on_recovertime_timeout():
+	ready = true
 
 func _on_minion_body_entered(body):
 	area_enemy = true
